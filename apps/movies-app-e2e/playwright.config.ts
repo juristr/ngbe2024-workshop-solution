da05a59904472b53d53c0022f9c1af47b0b8df8d
Juri <juri.strumpflohner@gmail.com>
2024-12-03 23:57:18 +0100

    enable atomizer

--- apps/movies-app-e2e/playwright.config.ts
+++ apps/movies-app-e2e/playwright.config.ts
@@ -4,8 +4,8 @@ import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
-
+// const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
+const baseURL = 'http://localhost:4200';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -17,6 +17,11 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
  */
 export default defineConfig({
   ...nxE2EPreset(__filename, { testDir: './src' }),
+  timeout: process.env['CI'] ? 120_000 : 60_000,
+  forbidOnly: !!process.env['CI'],
+  retries: process.env['CI'] ? 2 : 0,
+  workers: 1,
+  reporter: 'html',
   use: {
     baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
